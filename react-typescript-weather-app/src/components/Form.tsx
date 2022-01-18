d7a34689fdc848eeb8a1fc36e2509d3f952d7039
araobp <araobp@users.noreply.github.com>
2022-01-18 20:05:11 +0900

    Update Form.tsx

--- react-typescript-weather-app/src/components/Form.tsx
+++ react-typescript-weather-app/src/components/Form.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import { isReturnStatement } from "typescript";
 
 const Form=()=>{
-    const[city, setCity]=useState("");
+    const[city, setCity]=useState<string>("");
+    const getWeather=(e: any)=>{
+        e.preventDefault();
+        fetch("https://api.weatherapi.com/v1/current.json?key=97dd9644f14c4794aed13740221801&q=London&aqi=no")
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    }
     return(
         <form>
-            <input type="text" name="city" placeholder="City name"/>
-            <button type="submit">Get Weather</button>
+            <input type="text" name="city" placeholder="City name" onChange={e => setCity(e.target.value)}/>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
