021540cd3b579867e8efbd4e7aa1d4eb41775fca
araobp <araobp@users.noreply.github.com>
2022-01-22 22:06:49 +0900

    Update Results.js

--- myphotos/src/components/Results.js
+++ myphotos/src/components/Results.js
@@ -6,14 +6,12 @@ const Results = ({ records }) => {
                     <th>datetime</th>
                     <th>place</th>
                     <th>memo</th>
-                    <th>format</th>
                 </tr>
                 {records.map((r, i) => (
                     <tr>
                         <td>{r.datetime}</td>
                         <td>{r.place}</td>
                         <td>{r.memo}</td>
-                        <td>{r.format}</td>
                     </tr>
                 ))}
             </table>
