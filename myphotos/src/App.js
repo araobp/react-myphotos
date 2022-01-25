d827d3f0a807ba46e4974e391426313771b30f12
araobp <araobp@users.noreply.github.com>
2022-01-25 19:43:13 +0900

    update

--- myphotos/src/App.js
+++ myphotos/src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";  // named export
 import Title from './components/Title';
 import Form from './components/Form';
-import Results from './components/Results';
+import Records from './components/Records';
 import File from './components/File';
 import './App.css';
 
 const BASE_URL = "https://myphotos1088001.herokuapp.com";
 
 function App() {
-  
+
   const [id, setId] = useState({
-    id: 0 
+    id: 0
   });
 
   const [records, setRecords] = useState([]);
@@ -26,9 +26,11 @@ function App() {
 
   const getRecords = e => {
     e.preventDefault();
+
     fetch(`${BASE_URL}/records`)
       .then(res => res.json())
       .then(data => setRecords(data));
+    
   }
 
   const postRecord = e => {
@@ -36,14 +38,14 @@ function App() {
     console.log(recordInput);
 
     // POST /records
-    const body = { place: recordInput.place, memo: recordInput.memo};
+    const body = { place: recordInput.place, memo: recordInput.memo };
     const method = "POST";
     const headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     };
     console.log(body);
-    fetch(`${BASE_URL}/records`, {method, headers, body})
+    fetch(`${BASE_URL}/records`, { method, headers, body })
       .then(res => res.json())
       .then(console.log)
       .catch(console.error);
@@ -53,9 +55,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Title />
-        
+
         <Form setId={setId} getRecord={getRecord} getRecords={getRecords} />
-        <Results records={records} />
+        <Records BASE_URL={BASE_URL} records={records}/>
 
         <File recordInput={recordInput} setRecordInput={setRecordInput} postRecord={postRecord} />
       </header>
---
+++ myphotos/src/components/Records.js
@@ -0,0 +1,30 @@
+const Records = ({ BASE_URL, records}) => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>datetime</th>
+                        <th>place</th>
+                        <th>memo</th>
+                        <th>thumbnail</th>
+                    </tr>
+                </thead>
+                {records.map((r, i) => (
+                    <tbody key={i}>
+                        <tr>
+                            <td>{r.id}</td>
+                            <td>{new Date(r.record.datetime).toLocaleString()}</td>
+                            <td>{r.record.place}</td>
+                            <td>{r.record.memo}</td>
+                            <td><img src={`${BASE_URL}/photos/${r.id}/thumbnail`} /></td>
+                        </tr>
+                    </tbody>
+                ))}
+            </table>
+        </div>
+    );
+}
+
+export default Records;
