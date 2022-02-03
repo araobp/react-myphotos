6b7307f74fdd4984e358267e432e11d4a299721f
araobp <araobp@users.noreply.github.com>
2022-02-03 17:20:43 +0900

    Update auth.js

--- myphotos/src/util/auth.js
+++ myphotos/src/util/auth.js
@@ -1,6 +1,6 @@
 import { Base64 } from 'js-base64';
 
-const login = 'test';
-const password = 'passw0rd';
+const defaultLogin = localStorage.getItem("login") || 'test';
+const defaultPassword = localStorage.getItem("password") || 'passw0rd';
 
-export const authHeaders = {Authorization: `Basic ${Base64.encode(`${login}:${password}`)}`};
+export const authHeaders = {Authorization: `Basic ${Base64.encode(`${defaultLogin}:${defaultPassword}`)}`};
