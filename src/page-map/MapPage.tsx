3ac56b603844a1a8d40857e43957850c4d3fa9b8
araobp <araobp@users.noreply.github.com>
2022-02-23 07:33:09 +0900

    update

--- src/page-map/MapPage.tsx
+++ src/page-map/MapPage.tsx
@@ -6,18 +6,24 @@ const position: LatLngExpression = [51.505, -0.09];
 
 export const MapPage = () => {
     return (
-        <div style={{overflow: "hidden"}}>
-            <MapContainer center={position} zoom={15} scrollWheelZoom={true} id="map-height">
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
+        <>
+            <div style={{ overflow: "hidden" }}>
+                <MapContainer center={position} zoom={15} scrollWheelZoom={true} id="map-height">
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+            <div className="footer">
+                <button className="tiny-button" style={{ fontSize: "1.3rem" }} type="submit">&lt;</button>
+                <button className="tiny-button" style={{ fontSize: "1.3rem" }} type="submit">&gt;</button>
+            </div>
+        </>
     )
 }
